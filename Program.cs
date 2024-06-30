0b02c5db4d66788740e8a3a55bc5b7a1e5ab5d22
Fakeapate <imboden.samuel@protonmail.ch>
2024-06-30 11:57:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rednet_sec_lnk_datastream
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
